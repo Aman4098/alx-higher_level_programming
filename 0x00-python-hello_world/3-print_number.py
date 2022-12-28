235bbf33057edf7d85084189bfa305421951c235
Aman4098 <amanualdawit1978@gmail.com>
2022-12-28 08:16:26 -0800

    print

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# Author - Bamidele Adefolaju
-
-for letter in range(97, 123):
-
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+number = 98
+print(f"{number:d} Battery street")
